341b74a69a5a16e3a136dbfd60c436fd979cf849
igorG7 <igor.giovane.costa@gmail.com>
2026-02-03 15:29:38 -0300

    Configuração inicial

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+// .eslintrc.cjs
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        es2022: true,
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+    ],
+    rules: {
+        // suas regras personalizadas aqui
+        "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    },
+};
