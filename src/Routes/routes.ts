3415f90b663f2b2a4a3ffbfb80f330bb8ad465e2
igorG7 <igor.giovane.costa@gmail.com>
2026-02-06 15:11:21 -0300

    Adicionando rota 'customer' ao arquivo de rotas principais

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -3,12 +3,13 @@ import { Router } from "express";
 
 import userRoutes from "./user.routes";
 import pendingIssuesRoutes from "./pendingissues.routes";
+import customerRoutes from "./customer.routes";
 
 const routes = Router();
 
 // domínio / cadastros
-
 routes.use("/users", userRoutes);
 routes.use("/pendingissues", pendingIssuesRoutes);
+routes.use("/customer", customerRoutes);
 
 export default routes;
