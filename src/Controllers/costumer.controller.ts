9a981e2de3a4a7bc6d4edbfd5dc8f621be565fb9
igorG7 <igor.giovane.costa@gmail.com>
2026-02-08 12:44:47 -0300

    Criação do controller 'findCustomerByPartialName' - Customer Controller

--- src/Controllers/costumer.controller.ts
+++ src/Controllers/costumer.controller.ts
@@ -50,6 +50,44 @@ class CostumerController {
         .json({ message: "Erro interno inesperado.", error });
     }
   }
+
+  async findCustomerByPartialName(req: Request, res: Response) {
+    try {
+      const { name } = req.body;
+      Console({ type: "log", message: "Buscando clientes" });
+
+      const customers = await Customer.find(
+        {
+          full_name: { $regex: name, $options: "i" },
+        },
+        { password: 0 },
+      ).lean();
+
+      if (!customers.length) {
+        Console({ type: "warn", message: "Nenhum cliente encontrado." });
+        return res.status(404).json({
+          message: "Nenhum cliente encontrado.",
+          error: null,
+          data: [],
+        });
+      }
+
+      Console({
+        type: "success",
+        message: "Clientes encontrados com sucesso.",
+      });
+
+      return res.status(200).json({
+        message: "Clientes encontrados com sucesso.",
+        data: customers,
+      });
+    } catch (error) {
+      Console({ type: "error", message: "Erro interno inesperado." });
+      return res
+        .status(500)
+        .json({ message: "Erro interno inesperado.", error });
+    }
+  }
 }
 
 export default new CostumerController();
