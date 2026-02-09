3508ed9cd4b6ee8d49fb2d86eee532003aa03edd
igorG7 <igor.giovane.costa@gmail.com>
2026-02-09 10:58:46 -0300

    Documentação de 'validateFindCustomerByName' - Customer Middleware

--- src/Middlewares/Costumer/validateFindCustumerByName.ts
+++ src/Middlewares/Costumer/validateFindCustumerByName.ts
@@ -1,5 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 import Console from "../../Lib/Console";
+import { error } from "console";
+
+/**
+ * Middleware responsável por validar a chave enviada na busca de clientes.
+ * Regras de validação:
+ * - O campo `name` é obrigatório e deve estar presente no corpo da requisição.
+ *
+ * Comportamento:
+ * - Retorna HTTP 400 se o 'name' não for informado.
+ * - Retorna HTTP 400 se nenhuma informação de busca for enviada.
+ * - Caso todas as validações passem, a requisição segue para o próximo middleware
+ *
+ * @param {Request} req - Objeto da requisição do Express contendo o corpo com os dados de atualização.
+ * @param {Response} res - Objeto de resposta do Express.
+ * @param {NextFunction} next - Função que chama o próximo middleware da cadeia.
+ *
+ * @returns {Response | void} Retorna uma resposta HTTP 400 em caso de erro ou chama `next()` em caso de sucesso.
+ */
 
 export const validateFindCustomer = (
   req: Request,
@@ -10,7 +28,9 @@ export const validateFindCustomer = (
 
   if (!name) {
     Console({ type: "error", message: "Nenhum nome foi fornecido." });
-    return res.status(400).json({ message: "Nenhum nome foi fornecido." });
+    return res
+      .status(400)
+      .json({ message: "Nenhum nome foi fornecido.", error: null, data: [] });
   }
 
   next();
