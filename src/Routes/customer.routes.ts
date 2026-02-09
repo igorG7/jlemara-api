deb7be3b7cf7542ce95f96b093b01e224b7343f6
igorG7 <igor.giovane.costa@gmail.com>
2026-02-08 23:22:04 -0300

    Adicionando controller 'updateCustomer' no arquivo de rotas customer - Customer Routes

--- src/Routes/customer.routes.ts
+++ src/Routes/customer.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import Customer from "../Controllers/costumer.controller";
 
 import { validateFindCustomer } from "../Middlewares/Costumer/validateFindCustomer";
+import { validateCustomerUpdate } from "../Middlewares/Costumer/validateCustomerUpdate";
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.get("/find", validateFindCustomer, Customer.findCustomer);
 routes.get("/find/name", Customer.findCustomerByPartialName);
 routes.get("/", Customer.listAll);
 routes.get("/actives", Customer.listAllActiveCustomers);
+routes.patch("/update", validateCustomerUpdate, Customer.updateCustomer);
 
 export default routes;
