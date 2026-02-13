45a4bd568b4de5febfcdbc74a94b3ef6e8af4909
igorG7 <igor.giovane.costa@gmail.com>
2026-02-13 15:32:27 -0300

    Adição de rotas para utilizar os controllers 'findAddressCustomer' e 'findManyAddressCustomer' - Customer Routes

--- src/Routes/customer.routes.ts
+++ src/Routes/customer.routes.ts
@@ -4,17 +4,40 @@ import Customer from "../Controllers/costumer.controller";
 
 import { validateFindCustomer } from "../Middlewares/Costumer/validateFindCustomer";
 import { validateCustomerUpdate } from "../Middlewares/Costumer/validateCustomerUpdate";
+import { validateFindAddress } from "../Middlewares/Costumer/validateFindAddress";
+import { validateFindManyAddress } from "../Middlewares/Costumer/validateFindManyAddress";
 
 const routes = Router();
 
 routes.post("/create", Customer.createClient); // * Temporario
-routes.post("/search", validateFindCustomer, Customer.findCustomer);
-routes.post("/search/name", Customer.findCustomerByPartialName);
+
+// Rotas de busca (GET)
 routes.get("/page/:page/number/:number", Customer.listAll);
+
+routes.get(
+  "/address/:code_person",
+  validateFindAddress,
+  Customer.findAddressCustomer,
+);
+
 routes.get(
   "/actives/page/:page/number/:number",
   Customer.listAllActiveCustomers,
 );
+
+// Rotas de busca que utlizam filtros mais detalhadas (POST).
+// A utilização de POST de faz necessária para um envio de dados mais complexo para realizar a busca.
+routes.post("/search", validateFindCustomer, Customer.findCustomer);
+routes.post("/search/name", Customer.findCustomerByPartialName);
+
+routes.post(
+  "/addres",
+  validateFindManyAddress,
+  Customer.findManyAddressCustomer,
+);
+
+// Rotas de atualização (PUT/PATCH)
+
 routes.patch("/update", validateCustomerUpdate, Customer.updateCustomer);
 
 export default routes;
