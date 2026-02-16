8beb7db499ae7d7623cee7d5a03702f57d95510d
igorG7 <igor.giovane.costa@gmail.com>
2026-02-16 18:02:18 -0300

    Criação do controller 'listAll' - Development Controller

--- src/Controllers/develpment.controller.ts
+++ src/Controllers/develpment.controller.ts
@@ -15,6 +15,38 @@ class DevelopmentController {
       return res.status(500).json({ error });
     }
   }
+
+  async listAll(req: Request, res: Response) {
+    try {
+      const page = Number(req.params.page) || 1;
+
+      Console({ type: "log", message: "Buscando obras." });
+
+      const developments = await Development.find(
+        {},
+        {},
+        { limit: 5, skip: (page - 1) * 5 },
+      ).lean();
+
+      if (!developments.length) {
+        Console({ type: "warn", message: "Nenhuma obra encontrada." });
+        return res
+          .status(404)
+          .json({ message: "Nenhuma obra encontrada.", error: null, data: [] });
+      }
+
+      Console({ type: "success", message: "Busca por obras concluída." });
+
+      return res
+        .status(200)
+        .json({ message: "Busca por obras concluída.", data: developments });
+    } catch (error) {
+      Console({ type: "error", message: "Erro interno inesperado." });
+      return res
+        .status(500)
+        .json({ message: "Erro interno inesperado.", error });
+    }
+  }
 }
 
 export default new DevelopmentController();
