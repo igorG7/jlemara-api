9db0ef69fb86d5f269394e5d1faaaba314cbf078
igorG7 <igor.giovane.costa@gmail.com>
2026-02-26 14:57:16 -0300

    Adicionando rota para o controller "findUnits" - Unit Routes

--- src/Routes/unit.routes.ts
+++ src/Routes/unit.routes.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 
 import Unit from "../Controllers/unit.controller";
 
+import { validateFindUnits } from "../Middlewares/Unit/validateFindUnits";
+
 const routes = Router();
 
 routes.post("/create", Unit.createTemp);
 
+routes.post("/search", validateFindUnits, Unit.findUnits);
 routes.get("/avaliables", Unit.searchAvaliables);
 
 export default routes;
