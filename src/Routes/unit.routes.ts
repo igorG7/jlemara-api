dddee18385f960492594cc4d2add23a35e65c930
igorG7 <igor.giovane.costa@gmail.com>
2026-02-27 15:30:52 -0300

    Adicionando rotas para os controllers "addPhotos" e "removePhotos" - Unit Routes

--- src/Routes/unit.routes.ts
+++ src/Routes/unit.routes.ts
@@ -3,6 +3,8 @@ import { Router } from "express";
 import Unit from "../Controllers/unit.controller";
 
 import { validateFindUnits } from "../Middlewares/Unit/validateFindUnits";
+import { validatePhotoUnit } from "../Middlewares/Unit/validatePhotosUnit";
+import { validatePhotoDelete } from "../Middlewares/Unit/validateDeletePhoto";
 
 const routes = Router();
 
@@ -10,5 +12,8 @@ routes.post("/create", Unit.createTemp);
 
 routes.post("/search", validateFindUnits, Unit.findUnits);
 routes.get("/avaliables", Unit.searchAvaliables);
+
+routes.patch("/update/photo", validatePhotoUnit, Unit.addPhotos);
+routes.patch("/update/photo/remove", validatePhotoDelete, Unit.removePhoto);
 
 export default routes;
